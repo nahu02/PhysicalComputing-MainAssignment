38d2fe0121bdc32359840e810055bf473bc58a4e
betachess <williamjensenbc@gmail.com>
2025-11-27 11:03:53 +0100

    Updated comments and added if check for non-existing buttons in agent loop

--- agent_b/agent_b.ino
+++ agent_b/agent_b.ino
@@ -13,12 +13,12 @@ const uint8_t I2C_SELF_ADDRESS = 0x11;
 
 // Actuator/button pairs this agent controls
 ActuSensorator nodes[6] = {
-  {0x07, 10, 5},  // Actuator 0x05 on digi pin 10, button on pin 5
-  {0x08, 11, 6},  // Actuator 0x06 on digi pin 11, button on pin 6
-  {0x09, 12, 7},  // Actuator 0x07 on digi pin 12, button on pin 7
-  {0x0A, 13, 8},  // Actuator 0x08 on digi pin 13, button on pin 8
-  {0x0B, A2, -1}, // Red LED on analog pin A2
-  {0x0C, A3, -1}  // Green LED on analog pin A3
+  {0x07, 10, 5},  // Actuator 0x07 on digi pin 10, button on digi pin 5
+  {0x08, 11, 6},  // Actuator 0x08 on digi pin 11, button on digi pin 6
+  {0x09, 12, 7},  // Actuator 0x09 on digi pin 12, button on digi pin 7
+  {0x0A, 13, 8},  // Actuator 0x0A on digi pin 13, button on digi pin 8
+  {0x0B, A2, -1}, // Actuator 0x0B: Red LED on analog pin A2
+  {0x0C, A3, -1}  // Actuator 0x0C: Green LED on analog pin A3
 };
 
 #include "shared.h"
