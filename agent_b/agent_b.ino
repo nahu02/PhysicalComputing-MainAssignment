6cfd7ba3029f085239171b47fbc92341af28731a
betachess <williamjensenbc@gmail.com>
2025-11-27 10:50:57 +0100

    Made shared file for agents. Bring your own symbolic link.

--- agent_b/agent_b.ino
+++ agent_b/agent_b.ino
@@ -1,8 +1,5 @@
 #include <Wire.h>
 
-
-const uint8_t I2C_SELF_ADDRESS = 0x11;
-
 // Actuator/button pair data structure
 struct ActuSensorator {
   public:
@@ -12,6 +9,8 @@ struct ActuSensorator {
     int lastBtnState; // Tracking for edge detection
 };
 
+const uint8_t I2C_SELF_ADDRESS = 0x11;
+
 // Actuator/button pairs this agent controls
 ActuSensorator nodes[4] = {
   {0x05, 10, 5},  // Actuator 0x05 on digi pin 10, button on pin 5
@@ -20,75 +19,4 @@ ActuSensorator nodes[4] = {
   {0x08, 13, 8}   // Actuator 0x08 on digi pin 13, button on pin 8
 };
 
-// red A2
-// green A3
-
-bool isPhase1 = true;
-uint8_t directive;
-uint8_t mostRecentButtonPress = 0x00;
-
-void setup() {
-  // I2C
-  Wire.begin(I2C_SELF_ADDRESS);
-  TWAR = (I2C_SELF_ADDRESS << 1) | 1;  // enable broadcasts to be received
-  Wire.onReceive(receiveIndex);
-  Wire.onRequest(sendButtonPress);
-
-  // Pins
-  for (auto node : nodes) {
-    pinMode(node.buttonPin, INPUT);
-    pinMode(node.actuatorPin, OUTPUT);
-    digitalWrite(node.actuatorPin, LOW);
-  }
-}
-
-
-void receiveIndex(int bytes) {
-  directive = Wire.read();
-  switch (directive) {
-    case 0xF1:
-      isPhase1 = true;
-      mostRecentButtonPress = 0x00;
-      break;
-    case 0xF2:
-      isPhase1 = false;
-      mostRecentButtonPress = 0x00;
-      break;
-    case 0x00:
-      for (auto node : nodes) {
-        digitalWrite(node.actuatorPin, LOW);
-      }
-      break;
-    default:
-      if (isPhase1) {
-        for (auto node : nodes) {
-          if (node.id == directive) {
-            digitalWrite(node.actuatorPin, HIGH);
-          }
-        }
-      }
-      break;
-  }
-}
-
-void sendButtonPress() {
-  Wire.write((byte)mostRecentButtonPress);
-  mostRecentButtonPress = 0x00;
-}
-
-void loop() {
-
-  // Phase 2: Read button states
-  if (!isPhase1) {
-    for (auto& node : nodes) {
-      int buttonState = digitalRead(node.buttonPin);
-      // Edge detection: only trigger on button press
-      if (buttonState == HIGH && node.lastBtnState == LOW) {
-        mostRecentButtonPress = node.id;
-        delay(50);
-      }
-      node.lastBtnState = buttonState;
-    }
-  }
-}
-
+#include "shared.h"
