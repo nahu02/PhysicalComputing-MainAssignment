203c5ed75ad69f940c1573864a62a0294a9d35ec
nahu02 <nahu1002@gmail.com>
2025-12-01 20:59:51 +0100

    Update actuator/button pairs for best wiring

--- agent_a/agent_a.ino
+++ agent_a/agent_a.ino
@@ -13,12 +13,12 @@ const uint8_t I2C_SELF_ADDRESS = 0x10;
 
 // Actuator/button pairs this agent controls
 ActuSensorator nodes[6] = {
-  {0x01, 10, 5},  // Actuator 0x01 on digi pin 10, button on digi pin 5
-  {0x02, 11, 6},  // Actuator 0x02 on digi pin 11, button on digi pin 6
-  {0x03, 12, 7},  // Actuator 0x03 on digi pin 12, button on digi pin 7
-  {0x04, 13, 8},  // Actuator 0x04 on digi pin 13, button on digi pin 8
-  {0x05, A2, -1}, // Actuator 0x05: Red LED on analog pin A2
-  {0x06, A3, -1}  // Actuator 0x06: Green LED on analog pin A3
+  {0x01, 13, 5},
+  {0x02, 12, 6},
+  {0x03, 11, 7},
+  {0x04, 10, 8},
+  {0x05, A2, -1},
+  {0x06, A3, -1} 
 };
 
 #include "shared.h"
--- agent_b/agent_b.ino
+++ agent_b/agent_b.ino
@@ -2,21 +2,21 @@
 
 // Actuator/button pair data structure
 struct ActuSensorator {
-  public:
-    uint8_t id; // Global index of the actuator
-    int actuatorPin;
-    int buttonPin;
-    int lastBtnState; // Tracking for edge detection
+public:
+  uint8_t id; // Global index of the actuator
+  int actuatorPin;
+  int buttonPin;
+  int lastBtnState; // Tracking for edge detection
 };
 
 const uint8_t I2C_SELF_ADDRESS = 0x11;
 
 // Actuator/button pairs this agent controls
 ActuSensorator nodes[6] = {
-  {0x07, 10, 5},  // Actuator 0x07 on digi pin 10, button on digi pin 5
-  {0x08, 11, 6},  // Actuator 0x08 on digi pin 11, button on digi pin 6
-  {0x09, 12, 7},  // Actuator 0x09 on digi pin 12, button on digi pin 7
-  {0x0A, 13, 8},  // Actuator 0x0A on digi pin 13, button on digi pin 8
+  {0x07, 13, 5},  // Actuator 0x07 on digi pin 10, button on digi pin 5
+  {0x08, 12, 6},  // Actuator 0x08 on digi pin 11, button on digi pin 6
+  {0x09, 11, 7},  // Actuator 0x09 on digi pin 12, button on digi pin 7
+  {0x0A, 10, 8},  // Actuator 0x0A on digi pin 13, button on digi pin 8
   {0x0B, A2, -1}, // Actuator 0x0B: Red LED on analog pin A2
   {0x0C, A3, -1}  // Actuator 0x0C: Green LED on analog pin A3
 };
